4f6b3dd401fc7eeccb523e63a5b24e6f23642c10
jeichar <jesse.eichar@camptocamp.com>
2010-08-26 08:50:29 +0200

    URLs work to create streams for RamPath.  Also PathTests refactored so it is useable by both Ram and default filesystems

---
+++ scala-io/src/main/scala/scalax/io/ramfs/Handler.scala
@@ -0,0 +1,44 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2009-2010, Jesse Eichar          **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+package scalax.io.ramfs
+import java.net._
+import java.lang.Class
+
+class Handler extends URLStreamHandler {
+  def openConnection(url:URL) = {
+    require(url.getProtocol == RamFileSystem.protocol)
+    require(url.getHost contains "!")
+    
+    new RamURLConnection(url)
+  }
+}
+
+object Handler extends Handler
+
+class RamURLConnection(url:URL) extends URLConnection(url) {
+  lazy val path = {
+    val Array(id,path) = url.getHost.split("!")
+    RamFileSystem(id)(path+"/"+url.getPath)
+  }
+  def connect = {}
+
+  override def getInputStream = {
+    require(getDoInput, "getDoInput must be true")
+    path.ops.inputStream.open()
+  }
+
+  override def getOutputStream = {
+    require(getDoOutput, "getDoOutput must be true")
+    path.ops.outputStream().open()
+  }
+
+  override def getLastModified = path.lastModified
+
+  override def getContentLength = path.length.toInt
+}
---
+++ scala-io/src/test/scala/scalaio/test/URLStreamHandlerFactoryTest.scala
@@ -0,0 +1,26 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2009-2010, Jesse Eichar             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+package scalaio.test
+
+import scalax.io.PathURLStreamHandlerFactory
+import org.junit.Assert._
+import org.junit.{
+  Test, Ignore
+}
+import scalax.io.ramfs.RamFileSystem
+
+class URLStreamHandlerFactoryTest {
+  @Test
+  def testDefaultFactories :Unit = {
+    assertNotNull(PathURLStreamHandlerFactory createURLStreamHandler RamFileSystem.protocol)
+    // the following should return null so the default handlers will be used
+    assertNull(PathURLStreamHandlerFactory createURLStreamHandler "file")
+    assertNull(PathURLStreamHandlerFactory createURLStreamHandler "http")
+  }
+}
