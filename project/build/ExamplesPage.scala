eaf8eaa883ce8d13cd9111c929fe5438cb7e4273
jeichar <jesse.eichar@camptocamp.com>
2011-01-20 11:54:08 +0100

    adding specific pages for each example

---
+++ project/build/ExamplesPage.scala
@@ -0,0 +1,121 @@
+import sbt.{Logger, Path}
+import util.matching.Regex
+import xml.{NodeSeq, Elem, Node}
+import xsbt.FileUtilities._
+
+class ExamplesPage(val path:Path,logger:Logger, project:ProjectSite) {
+  val self = this;
+  private val text = read(path.asFile)
+  val base = path.name.take(path.name.size - ".scala".size)
+  val name = base.mkString.split("-").map{_.capitalize} mkString " "
+  val htmlName = base+".html"
+  private def cleanSummary(summary:String) = summary.replaceAll("(?m)^\\s*\\*\\s*","").replace("\n|\r"," ").trim
+  private def uberSummary(summary:String):String = summary.takeWhile(_ != '.') match {
+    case s if s.size > 100 => s.take(100).mkString+"..."
+    case s => s.mkString
+  }
+  val summary = {
+    val regex:Regex = """(?s)/\*\*(.+?)\*/\s*object""".r
+    val firstMatch = regex.findFirstMatchIn(text)
+    firstMatch.flatMap{
+      case matcher:Regex.Match if !matcher.before.toString.contains("object") =>
+        val summaryText = cleanSummary(matcher.group(1))
+        Some(summaryText)
+      case _ => None
+    }
+  }
+  val uberSummaryText:String = uberSummary(summary getOrElse "")
+  val examples = {
+    val objText = text.drop(text.indexOf("object"))
+    val regex:Regex = """(?s)/\*\*(.+?)\*/\s*def\s+(\S+)\s*=?\s*\{""".r
+    regex.findAllIn(objText).matchData.toList map {m =>
+      val name = m.group(2).flatMap {
+        case c if c.isUpperCase => " "+c
+        case c => c.toString
+      }.mkString.capitalize.trim
+
+      val summary = cleanSummary(m.group(1))
+      val (_,code:String) = ((1,"") /: m.after.toString) {
+        case ((0,text),_) => (0,text)
+        case ((1,text),'}') => (0,text)
+        case ((depth,text),'}') => (depth-1,text+'}')
+        case ((depth,text),'{') => (depth+1,text+'{')
+        case ((depth,text),c) => (depth,text+c)
+      }
+      val trimmedCode = {
+        def ws(l:String) = "^(\\s*)".r.findFirstMatchIn(l).get.group(1).size
+        var indent = 0
+        val lines = code.lines.foldLeft ("") {
+          case ("",l) =>
+            indent = ws(l)
+            l.substring(indent) + "\n"
+          case (acc,l) =>
+            acc + l.substring(Math.min(ws(l),indent)) + "\n"
+        }
+        lines mkString
+      }
+      Example(m.group(2)+".html",name,summary,uberSummary(summary), trimmedCode)
+    }
+  }
+
+  def pageNavList = {
+    <ul id="subnavlist">{for(ex <- examples) yield {
+      <li><a title={ex.uberSummary} href={ex.htmlName}>{ex.name}</a></li>
+    }}</ul>
+
+  }
+
+  def template(site:WebsiteModel)(exampleHtml:NodeSeq) = {
+    <html>
+      <head>
+        <title>{name}</title>
+        <script type="text/javascript" src="../js/shCore.js"></script>
+        <script type="text/javascript" src="../js/shBrushScala.js"></script>
+        <link href="../css/shCore.css" rel="stylesheet" type="text/css"></link>
+        <link href="../css/shThemeDefault.css" rel="stylesheet" type="text/css" ></link>
+        <link href="../css/samples.css" rel="stylesheet" type="text/css" ></link>
+      </head>
+      <body>
+        <div id="maincontainer">
+          <div id="topsection">
+            <div class="innertube">
+              <h1>{name}</h1>
+              {summary.map{s => <p class="summary">{s}</p>} getOrElse ""}
+            </div>
+          </div>
+
+          <div id="contentwrapper">
+            <div id="contentcolumn">
+              <div class="innertube">
+                {exampleHtml}
+              </div>
+            </div>
+          </div>
+
+          <div id="leftcolumn">
+            <div class="innertube">
+              {project.navbar(site, self)}
+            </div>
+          </div>
+
+          <div id="footer">
+            <a href="http://www.dynamicdrive.com/style/">Dynamic Drive CSS Library</a>
+          </div>
+        </div>
+
+        <script type="text/javascript">
+          SyntaxHighlighter.all()
+        </script>
+      </body>
+    </html>
+  }
+  def html(site:WebsiteModel) = {
+    template(site){
+      val html:NodeSeq = for(ex <- examples) yield {
+        ex.html
+      }
+      html
+    }
+  }
+
+}
--- project/build/WebiteModel.scala
+++ project/build/WebiteModel.scala
@@ -1,167 +1,93 @@
 import sbt.{Logger, Path}
-import util.matching.Regex
+import xml.Node
 import xsbt.FileUtilities._
 
-class ProjectSite(project:IoProject,logger:Logger) {
+object Printer {
+  def printPage(to:Path,html:Node,log:Logger) = {
+      log.info("Writing "+to.projectRelativePath)
+      val htmlWithDocType: String =
+        """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+         |"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+         |""".stripMargin+html
+      write(to.asFile, htmlWithDocType, utf8)
+    }
+}
+
+class ProjectSite(project:IoProject,log:Logger) {
+  val self = this;
   val name = project.name
   val description = "description TODO"
   val path = project.info.projectPath.name
-  def pagePath(page:Page) = path+"/"+page.htmlName
+  def pagePath(page:ExamplesPage) = path+"/"+page.htmlName
 
-  val pages = project.samplesSources.get map {path => new Page(path,logger)}
-}
+  val pages = project.samplesSources.get map {path => new ExamplesPage(path,log,self)}
 
-class Page(val path:Path,logger:Logger) {
-  private val text = read(path.asFile)
-  private val base = path.name.take(path.name.size - ".scala".size)
-  val name = base.mkString.split("-").map{_.capitalize} mkString " "
-  val htmlName = base+".html"
-  private def cleanSummary(summary:String) = summary.replaceAll("^\\s*\\*","").replace("\n|\r"," ")
-  val summary = {
-    val regex:Regex = """(?s)/\*\*(.+?)\*/\s*object""".r
-    val firstMatch = regex.findFirstMatchIn(text)
-    firstMatch.flatMap{
-      case matcher:Regex.Match if !matcher.before.toString.contains("object") =>
-        val summaryText = cleanSummary(matcher.group(1))
-        Some(summaryText)
-      case _ => None
-    }
-  }
-
-  val examples = {
-    val objText = text.drop(text.indexOf("object"))
-    val regex:Regex = """(?s)/\*\*(.+?)\*/\s*def\s+(\S+)\s*=?\s*\{""".r
-    regex.findAllIn(objText).matchData.toList map {m =>
-      val name = m.group(2).flatMap {
-        case c if c.isUpperCase => " "+c
-        case c => c.toString
-      }.mkString.capitalize.trim
-
-      val summary = cleanSummary(m.group(1))
-      val (_,code:String) = ((1,"") /: m.after.toString) {
-        case ((0,text),_) => (0,text)
-        case ((1,text),'}') => (0,text)
-        case ((depth,text),'}') => (depth-1,text+'}')
-        case ((depth,text),'{') => (depth+1,text+'{')
-        case ((depth,text),c) => (depth,text+c)
-      }
-      val trimmedCode = {
-        def ws(l:String) = "^(\\s*)".r.findFirstMatchIn(l).get.group(1).size
-        var indent = 0
-        val lines = code.lines.foldLeft ("") {
-          case ("",l) =>
-            indent = ws(l)
-            l.substring(indent) + "\n"
-          case (acc,l) =>
-            acc + l.substring(Math.min(ws(l),indent)) + "\n"
-        }
-        lines mkString
-      }
-      Example(m.group(2),name,summary,trimmedCode)
-    }
-  }
-}
-
-case class Example(href:String, name:String, summary:String, code:String) {
-  override def toString = "Example("+name+", "+summary.substring(0,10)+"..., "+code.substring(0,10)+"...)"
-}
-
-class WebsiteModel(projectSites:List[ProjectSite],outputDir:Path,log:Logger) {
-  def pageNavList(page:Page) = {
-    <ul id="subnavlist">{for(ex <- page.examples) yield {
-      <li><a href={"#"+ex.href}>{ex.name}</a></li>
-    }}</ul>
-
-  }
-  def navbar(currPage:Page) = {
-      <ul id="navlist">{for(project <- projectSites) yield {
-        <li>{project.name}
-          <ul>{for(page <- project.pages) yield {
-                if(currPage == page ){
-                  <li>
-                    <a id="active" href={"../"+project.pagePath(page)}>{page.name}</a>
-                    {pageNavList(page)}
-                  </li>
-                } else {
-                    <li><a href={"../"+project.pagePath(page)}>{page.name}</a></li>
-                }
-            }}</ul>
+  def navbar(site:WebsiteModel, currPage:ExamplesPage) = {
+    <div id="navcontainer">
+      <ul id="projectnavlist">{for(project <- site.projectSites) yield {
+        <li><a title={project.description} class={if(project == self)"active" else ""}>{project.name.capitalize}</a>
+          { if(project == self) {
+            <ul id="navlist">{for(page <- project.pages) yield {
+                  if(currPage == page ){
+                    <li>
+                      <a title={page.uberSummaryText} class="active" href={"../"+project.pagePath(page)}>{page.name}</a>
+                      {page.pageNavList}
+                    </li>
+                  } else {
+                      <li><a title={page.uberSummaryText} href={"../"+project.pagePath(page)}>{page.name}</a></li>
+                  }
+              }}</ul>
+            } else {
+              Nil
+            }
+          }
         </li>
       }}</ul>
+    </div>
   }
 
-  def pageTemplate(page:Page) = {
-    <html>
-      <head>
-        <title>{page.name}</title>
-        <script type="text/javascript" src="../js/shCore.js"></script>
-        <script type="text/javascript" src="../js/shBrushScala.js"></script>
-        <link href="../css/shCore.css" rel="stylesheet" type="text/css"></link>
-        <link href="../css/shThemeDefault.css" rel="stylesheet" type="text/css" ></link>
-        <link href="../css/samples.css" rel="stylesheet" type="text/css" ></link>
-      </head>
-      <body>
-        <div id="maincontainer">
-          <div id="topsection">
-            <div class="innertube">
-              <h1>{page.name}</h1>
-              {page.summary.map{s => <p class="summary">{s}</p>} getOrElse ""}
-            </div>
-          </div>
-
-          <div id="contentwrapper">
-            <div id="contentcolumn">
-              <div class="innertube">
-                {for(ex <- page.examples) yield {
-                  <div class="example">
-                    <a name={ex.href}><h3>{ex.name}</h3></a>
-                    <p class="example_summary">{ex.summary}</p>
-                    <div class="example_code">
-                      <pre class='brush: scala'>{ex.code.replace("<","&lt;")}</pre>
-                    </div>
-                  </div>}
-                }
-              </div>
-            </div>
-          </div>
-
-          <div id="leftcolumn">
-            <div class="innertube"  id="navcontainer">
-              {navbar(page)}
-            </div>
-          </div>
-
-          <div id="footer">
-            <a href="http://www.dynamicdrive.com/style/">Dynamic Drive CSS Library</a>
-          </div>
-        </div>
-
-        <script type="text/javascript">
-          SyntaxHighlighter.all()
-        </script>
-      </body>
-    </html>
-  }
 
   def projectTemplate(project:ProjectSite) {
 
   }
 
-  def printPage(to:Path,page:Page) = {
-    log.info("Writing "+page.name+" to "+to.projectRelativePath)
-    val html: String =
-      """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-       |"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-       |""".stripMargin+pageTemplate(page).toString
-    write(to.asFile, html,utf8)
+  def buildSite(site:WebsiteModel) = {
+    val projDir = site.outputDir / path
+    projDir.asFile.mkdirs
+    pages foreach{
+      p =>
+        val html = p.html(site)
+        val pageDir = projDir / p.base
+        Printer.printPage(pageDir / (p.htmlName+".html"),html,log)
+        p.examples foreach {
+          ex =>
+            val exHtml = p.template(site)(ex.html)
+            Printer.printPage(pageDir / (ex.htmlName),exHtml,log)
+        }
+    }
   }
+}
 
+case class Example(htmlName:String, name:String, summary:String, uberSummary:String, code:String) {
+  override def toString = "Example("+name+", "+summary.substring(0,10)+"..., "+code.substring(0,10)+"...)"
+  def html = {
+    <div class="example">
+      <a name={htmlName}><h3>{name}</h3></a>
+      <p class="example_summary">{summary}</p>
+      <div class="example_code">
+        <pre class='brush: scala'>{code.replace("<","&lt;")}</pre>
+      </div>
+    </div>
+  }
+}
+
+class WebsiteModel(val projectSites:List[ProjectSite],val outputDir:Path,log:Logger) {
+  val self = this;
   def buildSite = {
     projectSites.foreach {
       project=>
-        val projDir = outputDir / project.path
-        projDir.asFile.mkdirs
-        project.pages foreach{p => printPage(projDir / p.htmlName,p)}
+        project.buildSite(self);
     }
   }
+
 }
