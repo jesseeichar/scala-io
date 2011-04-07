f137e2e57d209103a35bb41a09ba953231110878
jeichar <jesse.eichar@camptocamp.com>
2011-04-07 23:38:45 +0200

    Migrated Application to App

--- web-site/src/main/scala/ScalaIOExample.scala
+++ web-site/src/main/scala/ScalaIOExample.scala
@@ -2,7 +2,7 @@ import java.net.URL
 import scalax.io.Input.asInputConverter
 import scalax.file.Path
 
-object ScalaIOExample extends Application {
+object ScalaIOExample extends App {
   val scalalang = new URL("http://www.scala-lang.org").asInput.bytes
   val scalatools = new URL("http://www.scala-tools.org").asInput.bytes
 
