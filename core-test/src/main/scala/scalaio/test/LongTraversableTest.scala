69390cb724bd22a2d54a34f77b0eacd56d1b3fa3
jeichar <jesse.eichar@camptocamp.com>
2011-04-11 08:09:51 +0200

    added some seq functions as per https://github.com/jesseeichar/scala-io/issues/17

--- core-test/src/main/scala/scalaio/test/LongTraversableTest.scala
+++ core-test/src/main/scala/scalaio/test/LongTraversableTest.scala
@@ -10,18 +10,22 @@ Test, Before, After, Rule, Ignore
 }
 
 class LongTraversableTest {
+  def traversable[U](tsize:Int)
+                    (implicit callback:(Int)=>U = (_:Int) => (),
+                     dataFunc: (Int) => Traversable[Int] = (i:Int) => 1 to i) =
+    new LongTraversable[Int] {
+      val data = dataFunc(tsize)
+      def foreach[U](f: (Int) => U) = data foreach {i =>
+        callback(i)
+        f(i)
+      }
+    }
+
   @Test
   def map_is_lazy_on_view = {
     var check = 0
 
-    val input = new LongTraversable[Int] {
-      val data = 1 to 3
-      def foreach[U](f: (Int) => U) = data foreach {i =>
-        check += 1
-        f(i)
-      }
-    }.view
-
+    val input = traversable(3)(_ => check += 1).view
 
     val mapped = input.map{ _ + 1}
 
@@ -32,13 +36,7 @@ class LongTraversableTest {
   @Test
   def limitedFold_only_processes_subset_of_elements = {
     var check = 1
-    val input = new LongTraversable[Int] {
-      val data = 1 to 100
-      def foreach[U](f: (Int) => U) = data foreach {i =>
-        check += 1
-        f(i)
-      }
-    }
+    val input = traversable(100)(_ => check += 1)
 
     val calculation = input.limitFold(0) {
       case (acc,i) if i < 50 => Continue(i + acc)
@@ -50,5 +48,102 @@ class LongTraversableTest {
     assertEquals(51,check)
     assertEquals(expected, calculation)
   }
+
+  @Test
+  def apply{
+    val input = traversable(100)
+    1 to 100 foreach { i =>
+      assertEquals(i,input(i-1))
+    }
+  }
+  @Test
+  def corresponds{
+    val input = traversable(100)
+    assertTrue(input.corresponds(1 to 100)( _ == _))
+    assertFalse(input.corresponds(1 to 101)( _ == _))
+    assertFalse(input.corresponds(0 to 100)( _ == _))
+    assertFalse(input.corresponds(0 to 99)( _ == _))
+    assertTrue(input.corresponds(1 to 100 map {_ => 300})( _ < _))
+  }
+  @Test
+  def indexWhere{
+    val input = traversable(100)
+    assertEquals(2,input.indexWhere(_ == 3))
+    assertEquals(6,input.indexWhere(_ > 3, 6))
+    assertEquals(-1,input.indexWhere(_ > 300))
+  }
+  @Test
+  def isDefinedAt{
+    val input = traversable(100)
+    assertEquals(2,input.indexWhere(_ == 3))
+    assertEquals(6,input.indexWhere(_ > 3, 6))
+    assertEquals(-1,input.indexWhere(_ > 300))
+  }
+  @Test
+  def indexOf{
+    val input = traversable(100)
+    assertEquals(-1, input.indexOf(200))
+    assertEquals(-1, input.indexOf(2,4))
+    assertEquals(1, input.indexOf(2))
+  }
+  @Test
+  def lastIndexOf{
+    val input = traversable(0)(_ => (), _ => List(1,2,3,4,4,3,2,1))
+    assertEquals(-1, input.lastIndexOf(200))
+    assertEquals(7, input.lastIndexOf(1))
+    assertEquals(0, input.lastIndexOf(1,3))
+    assertEquals(2, input.lastIndexOf(3,2))
+    assertEquals(4, input.lastIndexOf(4))
+  }
+  @Test
+  def lastIndexWhere{
+    val input = traversable(100)
+    assertEquals(99, input.lastIndexWhere(200 >))
+    assertEquals(20, input.lastIndexWhere(200 >,20))
+    assertEquals(-1, input.lastIndexWhere(1000 <))
+    assertEquals(1, input.lastIndexWhere(2 ==))
+  }
+  @Test
+  def segmentLength{
+    val input = traversable(100)
+    assertEquals(9, input.segmentLength(10 >))
+    assertEquals(9 - 3, input.segmentLength(10 >,3))
+    assertEquals(0, input.segmentLength(100 <))
+  }
+  @Test
+  def prefixLength{
+    val input = traversable(100)
+    assertEquals(9, input.prefixLength(10 >))
+    assertEquals(0, input.prefixLength(100 <))
+  }
+  @Test
+  def startsWith {
+    val input = traversable(100)
+    assertTrue(input.startsWith(1 to 55))
+    assertFalse(input.startsWith(0 to 55))
+  }
+  @Test
+  def indexOfSlice{
+    val input = traversable(100)
+    assertEquals(0,input.indexOfSlice(1 to 55))
+    assertEquals(2,input.indexOfSlice(3 to 55,2))
+    assertEquals(39,input.indexOfSlice(40 to 55,30))
+    assertEquals(-1,input.indexOfSlice(1 to 55,30))
+    assertEquals(-1,input.indexOfSlice(1 to 101,0))
+    assertEquals(0,input.indexOfSlice(Nil,0))
+  }
+  @Test
+  def containsSlice{
+    val input = traversable(100)
+    assertTrue(input.containsSlice(Nil))
+    assertTrue(input.containsSlice(1 to 55))
+    assertTrue(input.containsSlice(3 to 55,2))
+    assertTrue(input.containsSlice(40 to 55,30))
+    assertFalse(input.containsSlice(1 to 55,30))
+    assertFalse(input.containsSlice(1 to 101,0))
+
+  }
+
+
 
 }
--- core/src/main/scala/scalax/io/LongTraversableLike.scala
+++ core/src/main/scala/scalax/io/LongTraversableLike.scala
@@ -9,6 +9,8 @@
 package scalax.io
 
 import scala.collection._
+import mutable.Builder
+import util.control.Breaks._
 
 /**
  * The control signals for the limitFold method in [[scalax.io.LongTraversable]].
@@ -18,17 +20,17 @@ import scala.collection._
  * @param result the value to either return from method to to the next stage of the fold
  * @tparam A the type of Traversable this FoldResult can be used with
  */
-sealed abstract class FoldResult[+A](result:A)
+sealed abstract class FoldResult[+A](val result:A)
 
 /**
  * Signal indicating that the fold should continue to process another value
  */
-case class Continue[+A](result:A) extends FoldResult(result)
+case class Continue[+A](currentResult:A) extends FoldResult(currentResult)
 
 /**
  * Signal indicating that the fold should stop and return the contained result
  */
-case class End[+A](result:A) extends FoldResult(result)
+case class End[+A](endResult:A) extends FoldResult(endResult)
 
 
 /**
@@ -118,7 +120,6 @@ trait LongTraversableLike[+A, +Repr <: LongTraversableLike[A,Repr]] extends Trav
    */
   def ltake(n: Long) : Repr = {
     val b = newBuilder
-    import util.control.Breaks._
 
     breakable {
       var c = 0L
@@ -140,4 +141,353 @@ trait LongTraversableLike[+A, +Repr <: LongTraversableLike[A,Repr]] extends Trav
    * The long equivalent of Traversable.view(from,to)
    */
   def lview(from: Long, until: Long) : LongTraversableView[A,Repr] = this.view.lslice(from, until)
+
+
+  def apply (idx: Long):A = ldrop(idx).head
+
+  /** Tests whether every element of this $coll relates to the
+   *  corresponding element of another sequence by satisfying a test predicate.
+   *
+   *  @param   that  the other sequence
+   *  @param   p     the test predicate, which relates elements from both sequences
+   *  @tparam  B     the type of the elements of `that`
+   *  @return  `true` if both sequences have the same length and
+   *                  `p(x, y)` is `true` for all corresponding elements `x` of this $coll
+   *                  and `y` of `that`, otherwise `false`.
+   */
+  def corresponds[B](that: Seq[B])(p: (A,B) => Boolean): Boolean = {
+    val j = that.iterator
+    var result = true
+    breakable{
+      foreach { next =>
+        if(!j.hasNext || !p(next,j.next)) {
+          result = false
+          break
+        }
+      }
+    }
+
+    result && !j.hasNext
+  }
+
+  /** Finds index of first element satisfying some predicate.
+   *
+   *  $mayNotTerminateInf
+   *
+   *  @param   p     the predicate used to test elements.
+   *  @return  the index of the first element of this $coll that satisfies the predicate `p`,
+   *           or `-1`, if none exists.
+   */
+  def indexWhere(p: A => Boolean): Long = indexWhere(p, 0)
+
+  /** Finds index of the first element satisfying some predicate after or at some start index.
+   *
+   *  $mayNotTerminateInf
+   *
+   *  @param   p     the predicate used to test elements.
+   *  @param   from   the start index
+   *  @return  the index `>= from` of the first element of this $coll that satisfies the predicate `p`,
+   *           or `-1`, if none exists.
+   */
+  def indexWhere(p: A => Boolean, from: Long): Long = {
+    var i = from
+    breakable{
+      ldrop(from) foreach { next =>
+          if (p(next)) break
+        else i += 1
+      }
+      i = -1
+    }
+    i
+  }
+  def isDefinedAt ( idx : Long ) : Boolean = (idx >= 0) && (idx < lsize)
+  /** Finds index of first occurrence of some value in this $coll.
+   *
+   *  $mayNotTerminateInf
+   *
+   *  @param   elem   the element value to search for.
+   *  @tparam  B      the type of the element `elem`.
+   *  @return  the index of the first element of this $coll that is equal (wrt `==`)
+   *           to `elem`, or `-1`, if none exists.
+   *
+   *  @usecase def indexOf(elem: A): Int
+   */
+  def indexOf[B >: A](elem: B): Long = indexOf(elem, 0)
+
+  /** Finds index of first occurrence of some value in this $coll after or at some start index.
+   *
+   *  $mayNotTerminateInf
+   *
+   *  @param   elem   the element value to search for.
+   *  @tparam  B      the type of the element `elem`.
+   *  @param   from   the start index
+   *  @return  the index `>= from` of the first element of this $coll that is equal (wrt `==`)
+   *           to `elem`, or `-1`, if none exists.
+   *
+   *  @usecase def indexOf(elem: A, from: Int): Int
+   */
+  def indexOf[B >: A](elem: B, from: Long): Long = indexWhere(elem ==, from)
+
+  /** Finds index of last occurrence of some value in this $coll.
+   *
+   *  $willNotTerminateInf
+   *
+   *  @param   elem   the element value to search for.
+   *  @tparam  B      the type of the element `elem`.
+   *  @return  the index of the last element of this $coll that is equal (wrt `==`)
+   *           to `elem`, or `-1`, if none exists.
+   *
+   *  @usecase def lastIndexOf(elem: A): Int
+   */
+  def lastIndexOf[B >: A](elem: B): Long = lastIndexWhere(elem ==)
+
+  /** Finds index of last occurrence of some value in this $coll before or at a given end index.
+   *
+   *  @param   elem   the element value to search for.
+   *  @param   end    the end index.
+   *  @tparam  B      the type of the element `elem`.
+   *  @return  the index `<= end` of the last element of this $coll that is equal (wrt `==`)
+   *           to `elem`, or `-1`, if none exists.
+   *
+   *  @usecase def lastIndexOf(elem: A, end: Int): Int
+   */
+  def lastIndexOf[B >: A](elem: B, end: Long): Long = lastIndexWhere(elem ==, end)
+
+  /** Finds index of last element satisfying some predicate.
+   *
+   *  $willNotTerminateInf
+   *
+   *  @param   p     the predicate used to test elements.
+   *  @return  the index of the last element of this $coll that satisfies the predicate `p`,
+   *           or `-1`, if none exists.
+   */
+  def lastIndexWhere(p: A => Boolean): Long = lastIndexWhere(p, - 1)
+
+  /** Finds index of last element satisfying some predicate before or at given end index.
+   *
+   * Always takes linear time and traverses entire traversal
+   *
+   *  @param   p     the predicate used to test elements.
+   *  @return  the index `<= end` of the last element of this $coll that satisfies the predicate `p`,
+   *           or `-1`, if none exists.
+   */
+  def lastIndexWhere(p: A => Boolean, end: Long): Long = {
+    var last = -1L
+    var i = 0L;
+    breakable{
+      foreach{next =>
+        if(end > 0 && i > end) break
+        if(p(next)) last = i
+        i += 1
+      }
+    }
+    last
+  }
+  /** Computes length of longest segment whose elements all satisfy some predicate.
+   *
+   *  $mayNotTerminateInf
+   *
+   *  @param   p     the predicate used to test elements.
+   *  @param   from  the index where the search starts.
+   *  @return  the length of the longest segment of this $coll starting from index `from`
+   *           such that every element of the segment satisfies the predicate `p`.
+   */
+  def segmentLength ( p : (A) ⇒ Boolean , from : Long = 0 ) : Long =
+    ldrop(from).limitFold(0){
+      case (acc,next) if p(next) => Continue(acc + 1)
+      case (acc,next) => End(acc)
+    }
+
+  /** Returns the length of the longest prefix whose elements all satisfy some predicate.
+   *
+   *  @param   p     the predicate used to test elements.
+   *  @return  the length of the longest prefix of this $coll
+   *           such that every element of the segment satisfies the predicate `p`.
+   */
+  def prefixLength(p: A => Boolean) = segmentLength(p, 0)
+
+
+  /** Tests whether this $coll contains the given sequence at a given index.
+   *
+   * If the both the receiver object, <code>this</code> and
+   * the argument, <code>that</code> are infinite sequences
+   * this method may not terminate.
+   *
+   * @param  that    the sequence to test
+   * @param  offset  the index where the sequence is searched.
+   * @return `true` if the sequence `that` is contained in this $coll at index `offset`,
+   *         otherwise `false`.
+   */
+  def startsWith[B](that: Seq[B], offset: Long): Boolean = {
+    val j = that.iterator
+    breakable{
+      ldrop(offset) foreach {next =>
+        if(!j.hasNext || j.next != next) break
+      }
+    }
+    !j.hasNext
+  }
+
+  /** Tests whether this $coll starts with the given sequence.
+   *
+   * @param  that    the sequence to test
+   * @return `true` if this collection has `that` as a prefix, `false` otherwise.
+   */
+  def startsWith[B](that: Seq[B]): Boolean = startsWith(that, 0)
+
+
+  /** Finds first index where this $coll contains a given sequence as a slice.
+   *  $mayNotTerminateInf
+   *  @param  that    the sequence to test
+   *  @return  the first index such that the elements of this $coll starting at this index
+   *           match the elements of sequence `that`, or `-1` of no such subsequence exists.
+   */
+  def indexOfSlice[B >: A](that: Seq[B]): Long = indexOfSlice(that, 0)
+
+  /** Finds first index after or at a start index where this $coll contains a given sequence as a slice.
+   *  $mayNotTerminateInf
+   *  @param  that    the sequence to test
+   *  @param  from    the start index
+   *  @return  the first index `>= from` such that the elements of this $coll starting at this index
+   *           match the elements of sequence `that`, or `-1` of no such subsequence exists.
+   */
+  def indexOfSlice[B >: A](that: Seq[B], from: Long): Long =
+  if(that.isEmpty) from
+  else {
+    if (this.hasDefiniteSize && that.hasDefiniteSize)
+      LongTraversableLike.indexOf(thisCollection, 0L, lsize, that, 0, that.length, from)
+    else {
+      var i = from
+      var s: LongTraversable[A] = thisCollection ldrop i
+      while (!s.isEmpty) {
+        if (s startsWith that)
+          return i
+
+        i += 1
+        s = s.tail
+      }
+      -1
+    }
+  }
+
+  /** Tests whether this $coll contains a given sequence as a slice.
+   *  $mayNotTerminateInf
+   *  @param  that    the sequence to test
+   *  @return  `true` if this $coll contains a slice with the same elements
+   *           as `that`, otherwise `false`.
+   */
+  def containsSlice[B](that: Seq[B]): Boolean = indexOfSlice(that) != -1
+
+  /** Tests whether this $coll contains a given sequence as a slice.
+   *  $mayNotTerminateInf
+   *  @param  that    the sequence to test
+   *  @return  `true` if this $coll contains a slice with the same elements
+   *           as `that`, otherwise `false`.
+   */
+  def containsSlice[B](that: Seq[B],start:Long): Boolean = indexOfSlice(that,start) != -1
+
+
+  /*
+  Probably can be done but requires more work than I have time for
+
+  def lastIndexOfSlice[B >: A](that: Seq[B]): Int = lastIndexOfSlice(that, length)
+  def lastIndexOfSlice[B >: A](that: Seq[B], end: Int): Int =
+  def endsWith[B](that: Seq[B]): Boolean = {
+   */
+  /*
+  Don't think the following are possible without a lot of work
+  def diff [B >: A] (that: Seq[B]) : Repr
+  def distinct : Repr
+  def intersect [B >: A] ( that : Seq[B] ) : Repr
+   */
+
+}
+
+object LongTraversableLike {
+    /**  A KMP implementation, based on the undoubtedly reliable wikipedia entry.
+   *
+   *  @author paulp
+   *  @since  2.8
+   */
+  private def KMP[B](S: Seq[B], W: Seq[B]): Option[Int] = {
+    // trivial cases
+    if (W.isEmpty) return Some(0)
+    else if (W drop 1 isEmpty) return (S indexOf W(0)) match {
+      case -1 => None
+      case x  => Some(x)
+    }
+
+    val T: Array[Int] = {
+      val arr = new Array[Int](W.length)
+      var pos = 2
+      var cnd = 0
+      arr(0) = -1
+      arr(1) = 0
+      while (pos < W.length) {
+        if (W(pos - 1) == W(cnd)) {
+          arr(pos) = cnd + 1
+          pos += 1
+          cnd += 1
+        }
+        else if (cnd > 0) {
+          cnd = arr(cnd)
+        }
+        else {
+          arr(pos) = 0
+          pos += 1
+        }
+      }
+      arr
+    }
+
+    var m, i = 0
+    def mi = m + i
+
+    while (mi < S.size) {
+      if (W(i) == S(mi)) {
+        i += 1
+        if (i == W.size)
+          return Some(m)
+      }
+      else {
+        m = mi - T(i)
+        if (i > 0)
+          i = T(i)
+      }
+    }
+    None
+  }
+
+  /** Finds a particular index at which one sequence occurs in another sequence.
+   *  Both the source sequence and the target sequence are expressed in terms
+   *  other sequences S' and T' with offset and length parameters.  This
+   *  function is designed to wrap the KMP machinery in a sufficiently general
+   *  way that all library sequence searches can use it.  It is unlikely you
+   *  have cause to call it directly: prefer functions such as StringBuilder#indexOf
+   *  and Seq#lastIndexOf.
+   *
+   *  @param  source        the sequence to search in
+   *  @param  sourceOffset  the starting offset in source
+   *  @param  sourceCount   the length beyond sourceOffset to search
+   *  @param  target        the sequence being searched for
+   *  @param  targetOffset  the starting offset in target
+   *  @param  targetCount   the length beyond targetOffset which makes up the target string
+   *  @param  fromIndex     the smallest index at which the target sequence may start
+   *
+   *  @return the applicable index in source where target exists, or -1 if not found
+   */
+  private def indexOf[B](
+    source: LongTraversable[B], sourceOffset: Long, sourceCount: Long,
+    target: Seq[B], targetOffset: Int, targetCount: Int,
+    fromIndex: Long): Long = {
+      val toDrop = fromIndex max 0
+      val src = (source.lslice(sourceOffset, sourceCount) ldrop toDrop).toSeq
+      val tgt = target.slice(targetOffset, targetCount)
+
+      KMP(src, tgt) match {
+        case None    => -1
+        case Some(x) => x.toLong + toDrop
+      }
+  }
+
 }
